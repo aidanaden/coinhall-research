753dd73e740e4657361968213ff0df0becf843ad
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-05-17 11:57:51 +0800

    docs: update juno analysis with freq counter

---
+++ juno-vals/counter.py
@@ -0,0 +1,16 @@
+from collections import OrderedDict
+
+addr_dictionary = {}
+df = open("/Users/yusuf/proj/coinhall-research/juno-vals/delegator_address.csv")
+
+for line in df:
+    if line in addr_dictionary:
+        addr_dictionary[line] += 1
+    else:
+        addr_dictionary[line] = 1
+
+sorted_dict = dict(sorted(addr_dictionary.items(), key=lambda item: item[1]))
+
+for k, v in sorted_dict.items():
+    if v >= 2:
+        print(k.strip('\n'), ':' , v)
